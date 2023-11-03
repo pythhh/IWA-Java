84524336fbf7efef63775cc074dc9f559e3fcbfb
Pattaranan J <pattaranan.j@netpoleons.com>
2023-11-03 18:22:58 +0700

    CI: Test Scan SCSAST

--- devops-integrations/aws/scancentral-sast.bash
+++ devops-integrations/aws/scancentral-sast.bash
@@ -56,14 +56,14 @@ fcli ssc session list
 fcli sc-sast session login --ssc-url $FCLI_DEFAULT_SSC_URL -t $FCLI_DEFAULT_SSC_CI_TOKEN -c $FCLI_DEFAULT_SC_SAST_CLIENT_AUTH_TOKEN
 fcli sc-sast session list
 
-scancentral -help
 scancentral package -bt mvn -o package.zip
 
 #fcli sc-sast scan start --appversion=$ssc_app_version_id --upload --sensor-version=$scancentral_client_version --package-file=package.zip --store='?'
 fcli sc-sast scan start --publish-to=$ssc_app_version_id --sensor-version=$scancentral_client_version --ssc-ci-token $FCLI_DEFAULT_SSC_CI_TOKEN --package-file=package.zip
+echo Completing Submit Scan Request to ScanCentral SAST Controller
 
-fcli sc-sast scan wait-for --interval=30s
-# #fcli ssc appversion-vuln count --appversion=$SSC_APP_VERSION_ID
+#fcli sc-sast scan wait-for --interval=30s
+#fcli ssc appversion-vuln count --appversion=$SSC_APP_VERSION_ID
 
 echo Terminating connection with Fortify Platform
 fcli sc-sast session logout --no-revoke-token
